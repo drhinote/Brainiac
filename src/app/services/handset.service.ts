d2a66e591ce473df16ed8ec3805cb71f7e240911
drhinote <drhinote@gmail.com>
2019-07-03 08:21:32 -0500

    2 files modified

--- src/app/services/handset.service.ts
+++ src/app/services/handset.service.ts
@@ -1,7 +1,11 @@
 import { CordovaService } from './cordova.service';
 import {Observable} from 'rxjs';
 
-function addSample(sample, finger) {
+    function fix(val) {
+      return val-(Math.floor(val/8388608)*16777216);
+    }
+
+    function addSample(sample, finger) {
         if (finger.samples) finger.samples.push(sample);
     }
   
@@ -9,9 +13,9 @@ function addSample(sample, finger) {
         return ((total / count - finger.offset) / (finger.max - finger.offset) * 100).toFixed(1);
     }
 
-    function populateFinger(finger, data, idx) {
-        finger.offset = data[idx] * 65536 + data[idx + 1] * 256 + data[idx + 2];
-        finger.max = data[idx + 9] * 65536 + data[idx + 10] * 256 + data[idx + 11];
+    function populateFinger(finger, data, idx, isPadded) {
+        finger.offset = fix(data[idx] * 65536 + data[idx + 1] * 256 + data[idx + 2]);
+        finger.max = fix(data[idx + 9] * 65536 + data[idx + 10] * 256 + data[idx + 11]);
     }
 
     var handsetData = {
@@ -52,9 +56,8 @@ function Handset() {
   
   this.pinkyPct = 0;
 
-  this.ping = () => {
-  
-                this.cordova.native.serial.writeHex("AA0300002940", handsetInstance.ping, () => setTimeout(handsetInstance.init, 1000));
+  this.ping = () => {  
+                this.cordova.native.serial.writeHex("AA0300002940", () => setTimeout(handsetInstance.ping), () => setTimeout(handsetInstance.init, 1000));
   };
   
  this.init = () => {
@@ -62,12 +65,13 @@ function Handset() {
             this.cordova.native.serial.open({ baudRate: 460800 }, () => {
                 this.cordova.native.serial.registerReadCallback(
                     function success(data) {
-                        var view = new Uint8Array(data);
+                        var view = data;
                         if (view[1] === 2) {
-                            if (view[7] >= 77) {
-                                populateFinger(handsetData.thumb, view, 8);
-                                populateFinger(handsetData.index, view, 11);
-                                populateFinger(handsetData.pinky, view, 14);
+                            if (view[7] > 76) {
+                              
+                                populateFinger(handsetData.thumb, view, 8, view[7] == 78);
+                                populateFinger(handsetData.index, view, 11, view[7] == 78);
+                                populateFinger(handsetData.pinky, view, 14, view[7] == 78);
                                 if (view[7] > 77) {
                                     var t = "";
                                     for (var j = 26; view[j] !== 0; j++)
@@ -83,22 +87,22 @@ function Handset() {
                                 var ti = 0;
                                 var tp = 0;
                                 for (var i = 7; i < length * 9; i += 9) {
-                                    var ts = view[i] + view[i + 1] * 256 + view[i + 2] * 65536;
-                                    var is = view[i + 3] + view[i + 4] * 256 + view[i + 5] * 65536;
-                                    var ps = view[i + 6] + view[i + 7] * 256 + view[i + 8] * 65536;
+                                    var ts = fix(view[i] + view[i + 1] * 256 + view[i + 2] * 65536);
+                                    var ish = fix(view[i + 3] + view[i + 4] * 256 + view[i + 5] * 65536);
+                                    var ps = fix(view[i + 6] + view[i + 7] * 256 + view[i + 8] * 65536);
                                     tt += ts;
-                                    ti += is;
+                                    ti += ish;
                                     tp += ps;
 
                                     addSample(ts, handsetData.thumb);
-                                    addSample(is, handsetData.index);
+                                    addSample(ish, handsetData.index);
                                     addSample(ps, handsetData.pinky);
                                 }
 
-                                handsetInstance.thumbPct = handsetData.thumb.loadPercent = parseInt(toPercent(tt, length, this.handset.thumb));
-                                handsetInstance.indexPct = handsetData.index.loadPercent = parseInt(toPercent(ti, length, this.handset.index));
-                                handsetInstance.pinkyPct = handsetData.pinky.loadPercent = parseInt(toPercent(tp, length, this.handset.pinky));
-                             console.log(handsetInstance.thumbPct);
+                                handsetInstance.thumbPct = Math.min(Math.max(handsetData.thumb.loadPercent = parseInt(toPercent(tt, length, handsetData.thumb)),0),100);
+                                handsetInstance.indexPct = Math.min(Math.max(handsetData.index.loadPercent = parseInt(toPercent(ti, length, handsetData.index)),0),100);
+                                handsetInstance.pinkyPct = Math.min(Math.max(handsetData.pinky.loadPercent = parseInt(toPercent(tp, length, handsetData.pinky)),0),100);
+                                
                             }
 
                         }
